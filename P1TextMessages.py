354a87954703eb897853525bb37ef1e84f1a40db
Silverleaf140 <soojinclimenson@seattleacademy.org>
2025-09-10 18:37:35 +0000

    hi

--- P1TextMessages.py
+++ P1TextMessages.py
@@ -1,4 +1,15 @@
 print("Hello!")
 name = input("what is your name?")
-print(f"Hello {name}! today we are going to talk about you now! I'm going to ask you some questions.")
-print("What is your favorite color?")
+print(f"Hello {name}! today we are going to talk about you now! I'm going to ask you some questions. press enter to continue.")
+input("")
+color = input("What is your favorite color?")
+print(f"{color} is a cool color! I like the color blue!")
+input("")
+print ("what is your favorite animal?")
+input("")
+print ("Nice!")
+input("")
+("What types of food do you like?")
+
+
+
