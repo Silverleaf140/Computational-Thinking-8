73c14c2ea7229ea91e014680224bff3e88ccb066
Silverleaf140 <soojinclimenson@seattleacademy.org>
2025-11-05 16:25:55 +0000

    Code_art_1

---
+++ CodeArtIntro.py
@@ -0,0 +1,29 @@
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("blue")
+t.pendown()
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+
+# ###############################################
+# ### SETUP ###
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+
+
---
+++ P4_Code_Art2.py
@@ -0,0 +1,99 @@
+# Section 1 - Helper functions (DON'T CHANGE THIS!!)
+import turtle, math, time, random
+def set_background(image_filename):
+	screen = turtle.Screen()
+	try:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+	except:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+ 
+def set_image(sprite, image_filename):
+    image_file = f"./Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite.shape(image_file)
+ 
+def create_sprite(image_filename, x=0, y=0):
+    sprite = turtle.Turtle()
+    set_image(sprite, image_filename) 
+    sprite.penup()
+    sprite.goto(x,y)
+    window.update()
+    return sprite
+ 
+def get_distance(s1, s2):
+	dx = s1.xcor() - s2.xcor()
+	dy = s1.ycor() - s2.ycor()
+	return math.sqrt(dx*dx + dy*dy)
+ 
+def draw_rectangle( color="black",x=0,y=0, width=100, height=100,):
+	sprite = turtle.Turtle()
+	sprite.speed(0)
+	sprite.pencolor(color)
+	sprite.color(color)
+	sprite.penup()
+	sprite.goto(x - (width*0.5), y + (height*0.5))
+	sprite.pendown()
+	sprite.begin_fill()
+	for i in range(2):
+		sprite.forward(width)
+		sprite.right(90)
+		sprite.forward(height)
+		sprite.right(90) 
+	sprite.end_fill()
+	sprite.hideturtle()
+ 
+ 
+window = turtle.Screen()
+window.tracer(0)
+
+
+
+
+
+import turtle
+t = turtle.Turtle()
+
+#
+
+t.speed(10)
+t.goto(-180,0)
+turtle.Screen() .bgcolor ("azure")
+t.color("blue")
+
+#art code 
+# color sceme change for warm - cold
+colors = ["midnightblue","navy", "darkblue", "mediumblue", "blue", "royalblue", "dodgerblue", "deepskyblue", "skyblue", "lightskyblue", "powderblue"]
+
+message1 = create_sprite ("alien",-45,35)
+message1.color("aqua") 
+message1.write("CT8",font = ("Arial", 30, "normal", "bold"))
+message1.hideturtle()
+
+
+for i in range(500):
+	t.color( colors[i % 11 ] )
+	t.forward(350)
+	t.left (500 + 1)
+	t.hideturtle()
+
+window.update()
+
+turtle.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
